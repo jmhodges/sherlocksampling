74629104565aeb8e93424b431e2ef294ce8b5b74
deploy <jeff@somethingsimilar.com>
2008-03-09 23:26:34 -0700

    adding the Sampling migration and the schema.rb

--- db/migrate/001_create_samplings.rb
+++ db/migrate/001_create_samplings.rb
@@ -1,12 +1,15 @@
 class CreateSamplings < ActiveRecord::Migration
   def self.up
     create_table :samplings do |t|
-
+      t.string :uuid
       t.timestamps
     end
+    
+    add_index(:samplings, :uuid)
   end
 
   def self.down
     drop_table :samplings
+    remove_index(:samplings, :uuid)
   end
 end
