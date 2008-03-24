a3c4881d943edf2314fa6ed2a873fe8ee38f3532
Jeff Hodges <jhodges@Macintosh.local>
2008-03-23 17:37:29 -0700

    fixing up the bug tests for the new validations

--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Bug do
   before(:each) do
-    @bug = Bug.new
+    @bug = Bug.new(:line_number => 2, :problem_code => "FOooo")
   end
 
   it "should be valid" do
@@ -11,7 +11,10 @@ describe Bug do
   
   it "should be able to be called a duplicate" do
     @bug.save
-    @other_bug = Bug.create(:duplicate => @bug)
+    @other_bug = Bug.create(:duplicate => @bug,
+                            :line_number => @bug.line_number,
+                            :problem_code => @bug.problem_code
+    )
     @bug.should be_duplicate
     @bug.should_not be_original
   end
